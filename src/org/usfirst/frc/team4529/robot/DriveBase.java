f751fd6d6a9cbbda2f174207aaa55206b94097ea
Devon <froggerman330@gmail.com>
2016-02-08 10:34:35 +1100

    DriveBase interface for all drive bases to be able to do. Unimplemented.

---
+++ src/org/usfirst/frc/team4529/robot/DriveBase.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team4529.robot;
+
+/**
+ * Defines what each implementation of a drive base will be able to do.
+ * Mechanum, omniwheel, 6 wheel, 4 wheel, etc.
+ * 
+ * @author frogg
+ *
+ */
+public interface DriveBase
+{
+
+}
