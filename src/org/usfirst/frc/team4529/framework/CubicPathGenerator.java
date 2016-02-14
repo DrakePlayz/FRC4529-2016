bdb673cf0c52f7e8a81dec031d1944ea071ec373
Devon <froggerman330@gmail.com>
2016-02-15 10:40:52 +1100

    Fixed the classpath to be a relative reference to the new library so it will work on every system. Updated the cubic path generator to use matrices and added some comments on what still needs to happen with it.

--- src/org/usfirst/frc/team4529/framework/CubicPathGenerator.java
+++ src/org/usfirst/frc/team4529/framework/CubicPathGenerator.java
@@ -1,14 +1,13 @@
 package org.usfirst.frc.team4529.framework;
 
+import Jama.Matrix;
+
 public class CubicPathGenerator
 {
     private Position startingPosition;
     private Angle startingAngle;
     private Position endingPosition;
     private Angle endingAngle;
-
-    // y = Ax^3 + Bx^2 + Cx + D
-    private double A, B, C, D;
 
     public CubicPathGenerator(Position startingPosition, Angle startingAngle, Position endingPosition,
 	    Angle endingAngle)
@@ -28,18 +27,25 @@ public class CubicPathGenerator
 	double endingY = endingPosition.getY().getValue();
 	double endingGradient = endingAngle.getValue();
 
-	double[][] matrix = { { Math.pow(startingX, 3), Math.pow(startingX, 2), startingX, 1 },
+	double[][] coefficientMatrix = { { Math.pow(startingX, 3), Math.pow(startingX, 2), startingX, 1 },
 		{ Math.pow(startingX, 2) * 3, startingX * 2, 1, 0 },
 		{ Math.pow(endingX, 3), Math.pow(endingX, 2), endingX, 1 },
 		{ Math.pow(endingX, 2) * 3, endingX * 2, 1, 0 } };
 
-	double det = matrix[0][0] * matrix[1][1] * matrix[2][2] * matrix[3][3]
-		+ matrix[0][1] * matrix[1][2] * matrix[2][3] * matrix[3][0]
-		+ matrix[0][2] * matrix[1][3] * matrix[2][0] * matrix[3][1]
-		+ matrix[0][3] * matrix[1][0] * matrix[2][1] * matrix[3][2]
-		- matrix[0][3] * matrix[1][2] * matrix[2][1] * matrix[3][0]
-		- matrix[0][2] * matrix[1][1] * matrix[2][0] * matrix[3][3]
-		- matrix[0][1] * matrix[1][0] * matrix[2][3] * matrix[3][2]
-		- matrix[0][0] * matrix[1][3] * matrix[2][2] * matrix[3][1];
+	double[][] answerMatrix = { { startingY }, { startingGradient }, { endingY }, { endingGradient } };
+
+	Matrix matrixA = new Matrix(coefficientMatrix);
+	Matrix matrixB = new Matrix(answerMatrix);
+	Matrix matrixX = matrixA.inverse().times(matrixB);
+
+	System.out.println("y = " + matrixX.get(0, 0) + "x^3 + " + matrixX.get(1, 0) + "x^2 + " + matrixX.get(2, 0)
+		+ "x + " + matrixX.get(3, 0));
     }
+
+    // the double derivative of that solution can never be greater than our
+    // comfortable turning radius.
+    // the path can never go outside the bounds of the space we have (into
+    // restricted areas like the defenses).
+    // if no path is possible, get to the end point and go past, reverse to an
+    // angle close to then pull in to final point.
 }
